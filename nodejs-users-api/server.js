3fafa60efbc40fd086489cb3d0432f3bf60eecd5
amitvgi12 <52849460+amitvgi12@users.noreply.github.com>
2020-11-09 14:29:19 +0530

    Update server.js

--- nodejs-users-api/server.js
+++ nodejs-users-api/server.js
@@ -1,7 +1,10 @@
 
 
 
-
+var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase();
+var mongoHost = process.env[mongoServiceName + "_SERVICE_HOST"];
+var mongoPort = process.env[mongoServiceName + "_SERVICE_PORT"];
+var database = 'mongodb://'+process.env.MONGODB_USER+':'+process.env.MONGODB_PASSWORD+'@'+mongoHost+':'+mongoPort+'/'+process.env.MONGODB_DATABASE;
 var express 	= require('express');
 var app         = express();
 var bodyParser  = require('body-parser');
